62ef95cc9d2212826e14d230b438c1ddb28a5fb3
sulimanalhassan123as-code <sulimanalhassan.123.as@gmail.com>
2025-09-24 01:28:00 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const API_KEY = "YOUR_API_KEY"; // AIzaSyD4hutA0VN9wMgKuOY7PFox9mpRkzlOHv4
-const CX = "YOUR_CX"; // b3e9a30c9419d4590
+const API_KEY = AIzaSyD4hutA0VN9wMgKuOY7PFox9mpRkzlOHv4
+const CX = b3e9a30c9419d4590
 
 async function search() {
   const query = document.getElementById("searchBox").value.trim();
